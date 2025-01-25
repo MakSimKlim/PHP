7e1726615211500b7aa0721621f2e776dcf164a1
MakSimKlim <83maxomus@mail.ru>
2025-01-25 19:24:37 +0500

    PHP theory 4 Functions

--- Functions/index.php
+++ Functions/index.php
@@ -1,12 +1,18 @@
 <?PHP
+
+declare(strict_types=1); // Включаем строгую типизацию
+
 $title ='PHP Functions';
 
-function exchange(&$a, &$b)
-{
-	$buffer = $a;
-	$a =$b;
-	$b = $buffer;
-}
+//function exchange(&$a, &$b)
+//{
+//	$buffer = $a;
+//	$a =$b;
+//	$b = $buffer;
+//}
+// стрелочная функция
+$exchange = fn (&$a, &$b) => [$a, $b] = [$b, $a];
+
 
 function print_values($a, $b, $delimeter = ';')
 {
@@ -23,6 +29,52 @@ function sum_values(...$values)
 	}
 	return $sum;
 }
+function FillRand(&$arr, $size=5, $minRand=0, $maxRand=100)
+{
+	for($i=0; $i<$size; $i++){
+		$arr[$i] = rand($minRand, $maxRand);
+	}
+}
+function PrintArr($arr)
+{
+	echo'<pre>';
+	for($i=0; $i<count($arr); $i++){
+		echo "$arr[$i]\t";
+	}
+	echo'</pre>';
+}
+function increment()
+{
+	static $counter = 0;
+	$counter+=10;
+	return $counter;
+}
+
+// Включает строгую типизацию
+// declare(strict_types=1);
+
+function sum(int $a, int$b):int // | float | string | mixed
+{
+	return $a + $b;
+}
+//function sum(float $a, float $b): float
+//{
+//	return $a + $b;
+//}
+
+//Стрелочная функция
+$arrow_function = fn() => 'Hello I am arrow_function';
+$non_arrow_function = function () {return 'Hello I am not arrow_function';};
+
+//анонимная функция
+$PI = 3.14;
+//$circle_area = function (float $radius) use ($PI)
+//{
+//	return $PI * $radius * $radius;
+//};
+//анонимная стрелочная функция
+$circle_area = fn (float $radius) => $PI * $radius * $radius;
+
 
 require 'index.view.php';
 ?>
--- Functions/index.view.php
+++ Functions/index.view.php
@@ -10,9 +10,49 @@
     <?php
         [$a, $b] = [2, 3];
         print_values($a, $b, '.');
-        exchange($a, $b);
+        $exchange($a, $b);
         print_values($a, $b, '.');
         echo sum_values(3,5,8,13,21);
+
+        echo '<hr>';
+
+        $arr = array();
+        //FillRand($arr);
+        //FillRand(arr:$arr, maxRand:1000);
+        FillRand($arr, maxRand:1000);
+        PrintArr($arr);
+        echo '<hr>';
+
+        echo'<pre>';
+        //Суперглобальные переменные
+        //print_r($GLOBALS);
+        //print_r($_SERVER);
+        //$_GET
+        //$_POST
+        //print_r($_ENV);
+
+        //Статическая переменная
+        echo increment() . "\n";
+        echo increment() . "\n";
+        echo increment() . "\n"; 
+        echo'</pre>';
+        echo '<hr>';
+
+         echo'<pre>';
+
+        //Строготипизированные функции       
+        echo sum((int)2.7, (int)3.14);
+        echo '<hr>';
+
+        // Стрелочные функции
+        echo $arrow_function();
+        echo $non_arrow_function();
+        echo'</pre>';
+        echo '<hr>';
+
+        //анонимная функция
+        echo $circle_area(5);
+        echo '<hr>';
     ?>
 </body>
 </html>

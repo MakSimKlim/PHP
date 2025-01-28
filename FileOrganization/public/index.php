ec9170ab00aa8fa85722e59149f61fcd26917de3
MakSimKlim <83maxomus@mail.ru>
2025-01-28 18:30:23 +0500

    PHP theory 5 FileOrganizing

---
+++ FileOrganization/public/index.php
@@ -0,0 +1,7 @@
+﻿<?php require_once __DIR__.'/../inc/header.php';?>
+	<h1><?=TITLE?></h1>
+	<?= __FILE__ . '<br>' ?>
+	<pre>
+	<!--Directory_separator:<?= DIRECTORY_SEPARATOR;?>-->
+	</pre>
+<?php require_once __DIR__.'/../inc/footer.php';?>

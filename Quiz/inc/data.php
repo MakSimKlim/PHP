252adb53285780cad72f517b471dd57fa480d70a
MakSimKlim <83maxomus@mail.ru>
2025-02-07 05:42:41 +0500

    PHP theory 9 Quitz DONE

---
+++ Quiz/inc/data.php
@@ -0,0 +1,30 @@
+<?php
+$title = 'Quitz PD_321';
+
+$questions = 
+[
+	'Кто создал игру Max Payne?',
+	'На каком движке создано Crysis 2?',
+	'Как зовут главного персонажа Vice City?',
+	'В каком году вышла GTA-V?',
+	'На каком движке создано GTA-V?',
+];
+
+$answers = 
+[
+	['Bethesda', 'Crytek', 'Rockstar', 'Remady'],
+	['Cryengine 3', 'Cryengine 5', 'Rage'],
+	['Max Payne', 'Tommy Vercetty', 'Ricardo Diaz'],
+	['2012', '2013', '2015', '2016'],
+	['Cryengine 3', 'Cryengine 5', 'Rage'],
+];
+
+$correct_answers = 
+[
+	3,
+	0,
+	3,
+	1,
+	2,
+];
+?>
---
+++ Quiz/inc/login.php
@@ -0,0 +1,8 @@
+<form class="login-card" action="inc/quitz.php" method="post">
+	<div class="login-item"><h3>Enter your name:</h3></div>
+	<div class="login-item"><label>Last name:</label>	<input type="string" name="last_name"	id="last_name"		placeholder="LastName">	 </div>
+	<div class="login-item"><label>First name:</label>	<input type="string" name="first_name"	id="first_name"		placeholder="FirstName"> </div>
+	<div class="login-item"><label>Middle name:</label> <input type="string" name="middle_name" id="middle_name"	placeholder="MiddleName"></div>
+	<div class="login-item"><label>E-mail:</label>		<input type="email"	 name="email"		id="email"			placeholder="E-mail">	 </div>
+	<div class="login-item"><label> </label><input type="submit" value="Start"></div>
+</form>

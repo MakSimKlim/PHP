eb3569b3d87b882ca4e688e1dc16c278f70b997a
MakSimKlim <83maxomus@mail.ru>
2025-02-18 15:24:26 +0500

    Loading a file type 201 RAW.txt and creating from it files '201 ready.txt' and '201.dhcpd' DONE

---
+++ Network2/inc/upload.php
@@ -0,0 +1,35 @@
+﻿<?php
+require_once '../functions.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_FILES['file']) && $_FILES['file']['type'] === 'text/plain') {
+        $uploadDir = '../files/';
+        $sourceFile = $uploadDir . basename($_FILES['file']['name']);
+
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $sourceFile)) {
+            echo 'Файл успешно загружен.<br>';
+
+            // Создание двух новых файлов
+            $filenames = get_filenames($sourceFile);
+
+            $IPs = [];
+            $MACs = [];
+            read_adresses_from_file($filenames['source'], $IPs, $MACs);
+            write_adresses_from_file($filenames['WAL'], $IPs, $MACs);  // Сохранение в ready файл
+            write_adresses_from_file($filenames['DHCP'], $IPs, $MACs); // Сохранение в dhcpd файл
+            write_adresses_from_file($filenames[' ready.txt'], $IPs, $MACs); // Сохранение в txt файл
+
+            // Вывод содержимого новых файлов на страницу
+            echo '<h3>Содержимое файла ' . basename($filenames[' ready.txt']) . ' (таблица):</h3>';
+            print_table_from_file($filenames[' ready.txt']);
+
+            echo '<h3>Содержимое файла ' . basename($filenames['DHCP']) . ':</h3>';
+            echo '<pre>' . htmlspecialchars(file_get_contents($filenames['DHCP'])) . '</pre>';
+        } else {
+            echo 'Загрузка файла не удалась.';
+        }
+    } else {
+        echo 'Разрешена загрузка только .txt файлов.';
+    }
+}
+?>
---
+++ Network2/index.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+    <title>Upload file</title>
+</head>
+<body>
+    <main>
+        <form enctype="multipart/form-data" action="inc/upload.php" method="post">
+            <div>
+                <label for="file">Выберите файл:</label>
+                <input type="file" id="file" name="file" accept=".txt"/>
+            </div>
+            <div>
+                <input type="submit" value="Upload"/>
+            </div>
+        </form>
+    </main>
+</body>
+</html>

b5deefda7933b4301e9ed1e78873683a9faf33b0
MakSimKlim <83maxomus@mail.ru>
2025-03-04 23:14:11 +0500

    Inheritance in PHP

--- OOP/inc/body.php
+++ OOP/inc/body.php
@@ -1,73 +1,82 @@
 <?php
+require_once __DIR__ .'/point.php';
+require_once __DIR__ .'/inheritance.php';
 
-class Point
+//$A = new Point();
+////$A->set_x(1024);
+////$A->set_y(2048);
+//echo "Х = {$A->get_x()},\tY = {$A->get_y()};<br>";
+////echo $A->y;
+//$A->info();
+//
+//$B = new Point(2,3);
+//$B->info(); // вывод явным вариантом через стандартный класс
+//echo $B;    // вывод стандартный неявный вывод объекта $B через метод tostring
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+/*$human = new Human('Antonio', 'Montana', 25);
+echo $human.'<hr>';
+
+$student = new Student('Jessie', 'Pinkman', 25, 'Chemistry', 'WW_220', 95, 98);
+echo $student.'<hr>';
+
+$teacher = new Teacher('James', 'Hetfield', 62, 'Rock Musiciant', 45);
+echo $teacher.'<hr>';
+
+$graduate = new Graduate('Hank', 'Schreder', 40, 'Criminalistic', 'OBN', 80, 60 , 'How to catch Heissenberg');
+echo $graduate.'<hr>';*/
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+$group =
+[
+	new Student('Jessie', 'Pinkman', 25, 'Chemistry', 'WW_220', 95, 98),
+	new Teacher('James', 'Hetfield', 62, 'Rock Musiciant', 45),
+	new Graduate('Hank', 'Schreder', 40, 'Criminalistic', 'OBN', 80, 60 , 'How to catch Heissenberg'),
+	new Student('Tommy', 'Vercetty', 30, 'Theft', 'Vice', 95, 98),
+	new Teacher('Recardo', 'Diaz', 55, 'Weapons distribution', 20)
+];
+echo '<pre>';
+//echo get_class($group[0]);
+//echo get_class($group[1]);
+//echo get_class($group[2]);
+
+//echo '<hr>';
+//var_dump($group[0]);
+//echo '<hr>';
+//var_dump($group[1]);
+//echo '<hr>';
+//var_dump($group[2]);
+//echo '<hr>';
+echo '</pre>';
+
+echo '<table>';
+echo '<tr>';
+echo '<th>First name</th><th>Last name</th><th>Age</th><th>Speciality</th><th>Info</th>';
+echo '</tr>';
+for($i=0; $i < count($group); $i++)
 {
-	private $x;
-	private $y;
-	//protected $z;
-
-	function get_x()
-	{
-		return $this->x;
-	}
-	function get_y()
-	{
-		return $this->y;
-	}
-	function set_x($x)
-	{
-		$this->x = $x;
-	}
-	function set_y($y)
-	{
-		$this->y = $y;
-	}
-
-	// Constructors / Destructor
-
-	//function __construct()
+	//echo $group[$i] . '<hr>';
+	echo '<tr>';
+	echo "<td>{$group[$i]->get_first_name()}</td>";
+	echo "<td>{$group[$i]->get_last_name()}</td>";
+	echo "<td>{$group[$i]->get_age()}</td>";
+	echo "<td>{$group[$i]->get_speciality()}</td>";
+	echo '<td>';
+	//if(get_class($group[$i])==='Student')
 	//{
-	//
+	//	//echo "{$group[$i]->get_group()}, rating:{$group[$i]->get_rating()}, attendance:{$group[$i]->get_attendance()}";
+	//	echo $group[$i]->info();
 	//}
-
-	function __construct($x=0, $y=0)
-	{
-		$this->set_x($x);
-		$this->set_y($y);
-		echo "Constructor:\t<br>";
-	}
-	function __destruct()
-	{
-		
-		echo "Destructor:\t<br>";
-	}
-
-	function info()
-	{
-		if($this->x == null && $this->y == null)
-			echo 'Point is uninitialized';
-		else 
-			echo "X = {$this->x},\tY = {$this->y};";
-		echo "<hr>";
-	}
-	function __tostring()
-	{
-		if($this->x == null && $this->y == null)
-			return 'Point is uninitialized<br>';
-		else 
-			return "X = {$this->x},\tY = {$this->y};<br>";
-		
-	}
+	//if(get_class($group[$i]) === 'Teacher')
+	//{
+	//	echo $group[$i]->info();
+	//}
+	echo $group[$i]->info();
+	echo '</td>';
+	echo '</tr>';
 }
+echo '</table>';
 
-$A = new Point();
-//$A->set_x(1024);
-//$A->set_y(2048);
-echo "Х = {$A->get_x()},\tY = {$A->get_y()};<br>";
-//echo $A->y;
-$A->info();
-
-$B = new Point(2,3);
-$B->info(); // вывод явным вариантом через стандартный класс
-echo $B;    // вывод стандартный неявный вывод объекта $B через метод tostring
 ?>
---
+++ OOP/inc/inheritance.php
@@ -0,0 +1,224 @@
+﻿<?php
+
+class Human
+{
+	private $last_name;
+	private $first_name;
+	private $age;
+
+	function get_last_name()
+	{
+		return $this->last_name;
+	}
+	function get_first_name()
+	{
+		return $this->first_name;
+	}
+	function get_age()
+	{
+		return $this->age;
+	}
+
+	function set_last_name($last_name)
+	{
+		$this->last_name = $last_name;
+	}
+	function set_first_name($first_name)
+	{
+		$this->first_name = $first_name;
+	}
+	function set_age($age)
+	{
+		$this->age = $age;
+	}
+
+	// Constructors:
+	function __construct($first_name, $last_name, $age)
+	{
+		$this->set_first_name($first_name);
+		$this->set_last_name($last_name);
+		$this->set_age($age);
+		echo 'HConstructor<br>';
+	}
+	function __destruct()
+	{
+		echo 'HDestructor<br>';
+	}
+
+	function info()
+	{		
+	}
+
+	// вывод
+	function __tostring()
+	{
+		return "{$this->get_first_name()} {$this->get_last_name()} {$this->get_age()}";
+	}
+
+	
+}
+
+///////////////////////////////////////////////////////////////////////
+
+class Student extends Human
+{
+	private $speciality;
+	private $group;
+	private $rating;
+	private $attendance;
+
+	function get_speciality()
+	{
+		return $this->speciality;
+	}
+	function get_group()
+	{
+		return $this->group;
+	}
+	function get_rating()
+	{
+		return $this->rating;
+	}
+	function get_attendance()
+	{
+		return $this->attendance;
+	}
+
+	function set_speciality($speciality)
+	{
+		$this->speciality = $speciality;
+	}
+	function set_group($group)
+	{
+		$this->group = $group;
+	}
+	function set_rating($rating)
+	{
+		$this->rating = $rating;
+	}
+	function set_attendance($attendance)
+	{
+		$this->attendance = $attendance;
+	}
+	// Constructors:
+	function __construct($first_name, $last_name, $age, $speciality, $group, $rating, $attendance)
+	{
+		parent::__construct($first_name, $last_name, $age);
+		$this->set_speciality($speciality);
+		$this->set_group($group);
+		$this->set_rating($rating);
+		$this->set_attendance($attendance);
+		echo 'SConctructor<br>';
+	}
+	function __destruct()
+	{
+		echo 'SDestructor<br>';
+	}
+		function info()
+	{	
+		return "Group:{$this->get_group()}, Rating:{$this->get_rating()}, Attendance:{$this->get_attendance()}";
+	}
+
+	// вывод
+	function __tostring()
+	{
+		//echo '<pre>';
+		//print_r(__CLASS__ . '<br>');
+		//var_dump($this);
+		//echo '</pre>';
+
+		return parent::__tostring() . " {$this->get_speciality()} {$this->get_group()} {$this->get_rating()} {$this->get_attendance()}";
+	}
+
+}
+//////////////////////////////////////////////////////////////////
+
+class Graduate extends Student
+{
+	private $subject;
+
+	function get_subject()
+	{
+		return $this->subject;
+	}
+	function set_subject($subject)
+	{
+		$this->subject=$subject;
+	}
+	//Constructors:
+	function __construct($first_name, $last_name, $age, $speciality, $group, $rating, $attendance, $subject)
+	{
+		parent::__construct($first_name, $last_name, $age, $speciality, $group, $rating, $attendance);
+		$this->set_subject($subject);
+		echo '<pre>GConstructor</pre>';
+	}
+	function __destruct()
+	{
+		echo '<pre>GDestructor</pre>';
+	}
+
+	function info()
+	{	
+		return parent::info() . " Subject:{$this->get_subject()}";
+	}
+
+		// вывод
+	function __tostring()
+	{
+		return parent::__tostring() . " {$this->get_subject()}";
+	}
+
+}
+/////////////////////////////////////////////////////////////////
+class Teacher extends Human
+{
+	private $speciality;
+	private $experience;
+
+	function get_speciality()
+	{
+		return $this->speciality;
+	}
+	function get_experience()
+	{
+		return $this->experience;
+	}
+
+	function set_speciality($speciality)
+	{
+		$this->speciality = $speciality;
+	}
+	function set_experience($experience)
+	{
+		$this->experience = $experience;
+	}
+
+	//Constructors:
+
+	function __construct($first_name, $last_name, $age, $speciality, $experience)
+	{
+		parent::__construct($first_name, $last_name, $age);
+		$this->set_speciality($speciality);
+		$this->set_experience($experience);
+		echo 'TConctructor<br>';
+	}
+	function __destruct()
+	{
+		echo 'TDestructor<br>';
+	}
+	function info()
+	{	
+		return " Experience:{$this->get_experience()}";
+	}
+
+
+	// вывод
+	function __tostring()
+	{
+		return parent::__tostring() . " {$this->get_speciality()} {$this->get_experience()}<br>";
+	}
+
+
+}
+
+?>

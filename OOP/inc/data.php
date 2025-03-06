12ff1a66d066c5af9cdd87e72baf60fdd53f8bf3
MakSimKlim <83maxomus@mail.ru>
2025-03-06 18:28:59 +0500

    Read & Write in_from file with serialize DONE

--- OOP/inc/data.php
+++ OOP/inc/data.php
@@ -1,4 +1,11 @@
 <?php
-$group = [];
+$group =
+[
+	new Student('Jessie', 'Pinkman', 25, 'Chemistry', 'WW_220', 95, 98),
+	new Teacher('James', 'Hetfield', 62, 'Rock Musiciant', 45),
+	new Graduate('Hank', 'Schreder', 40, 'Criminalistic', 'OBN', 80, 60 , 'How to catch Heissenberg'),
+	new Student('Tommy', 'Vercetty', 30, 'Theft', 'Vice', 95, 98),
+	new Teacher('Recardo', 'Diaz', 55, 'Weapons distribution', 20)
+];
 
 ?>

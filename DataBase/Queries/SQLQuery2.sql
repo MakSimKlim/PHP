bc89941560b48fc0ef9bb08619e05eed8585aac7
MakSimKlim <83maxomus@mail.ru>
2025-03-15 20:00:03 +0500

    Add bases and Queries in DataBase folder

--- DataBase/Queries/SQLQuery2.sql
+++ DataBase/Queries/SQLQuery2.sql
@@ -1,10 +1,13 @@
-SELECT	
+USE PD_212;
+GO
+
+SELECT 
 		teacher_id,
 		FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS 'full_name',
 		birth_date,
 		DATEDIFF(DAY, work_since, GETDATE())/365 AS 'experience',
 		discipline_name
 FROM	Disciplines
-JOIN	TeachersDisciplinesRelation	ON	(discipline=discipline_id)
-JOIN	Teachers					ON	(teacher=teacher_id)
-WHERE	discipline_id = 21
+JOIN	TeachersDisciplinesRelation ON (discipline=discipline_id)
+JOIN	Teachers ON (teacher=teacher_id)
+WHERE	discipline_name LIKE N'%C++%';

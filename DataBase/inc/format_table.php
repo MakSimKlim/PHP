f0b48ef279f2b20fe3b61819aab1853d57cc3168
MakSimKlim <83maxomus@mail.ru>
2025-03-20 18:08:05 +0500

    get_teacher_from DB DONE

--- DataBase/inc/format_table.php
+++ DataBase/inc/format_table.php
@@ -5,17 +5,17 @@ function format_table($result)
 	$row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
 	while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
 	{
-		echo '<tr>';
+		echo "<tr id=\"{$row['teacher_id']}\">";
 		foreach($row as $col)
 		{
 			echo'<td>';
 			echo $col;
 			echo'</td>';
 		}
-		//echo '<td>';
-		//
-		//echo '</td>';
-		echo '</tr>';
+		echo '<td>';
+		echo "<a href=\"teacher.php?id={$row['teacher_id']}\">Подробно</a>";
+		echo '</td>';
+		echo "</tr>";
 	}
 }
 

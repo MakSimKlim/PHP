af006537619045cf9f0ef7ff0413584beec301f6
MakSimKlim <83maxomus@mail.ru>
2025-03-29 14:32:42 +0500

    Add teacher for discipline in progress

--- DataBase/inc/discipline.php
+++ DataBase/inc/discipline.php
@@ -1,4 +1,5 @@
 ﻿<?php
+//require_once __DIR__ . '/header.php';
 require_once __DIR__ . '/connection.php';
 echo '<pre>';
 print_r($_REQUEST);
@@ -17,17 +18,32 @@ echo '</pre>';
 		echo '<pre>';
 		//print_r($row);
 		echo '</pre>';
-
 		echo "<h1>{$row['discipline_name']}</h1>";
 		echo "Количество занятий: {$row['number_of_lessons']}";
 		echo '<h3>Преподаватели:</h3>';
-		echo '<table>';
-		echo '<thead>';
-		echo '<tbody id="table-teachers">';
-		require_once __DIR__ . '/get_teachers_for_discipline.php';
-		echo '</tbody>';
-		echo '</thead>';
-		echo '</table>';
+		echo '<div class="table-with-combo" style="display:flex;justify-content:space-between;">';
+			echo '<div>';
+				echo '<table>';
+				echo '<thead>';
+				echo '</thead>';
+				echo '<tbody id="table-teachers">';
+				require_once __DIR__ . '/get_teachers_for_discipline.php';
+				echo '</tbody>';
+				echo '</table>';
+			echo '</div>';
+
+			echo '<div style="margin-right:25px;display:block;text-align:right;">';
+				echo '<select>';
+				echo '<option value="0">';
+				echo '--- Добавить преподавателя ---';
+				echo '</option>';
+				require_once __DIR__ . '/get_teachers_to_combo_box.php';
+				echo '</select>';
+				echo '<br>';
+				echo '<input type="button" value="Добавить" style="margin-top:10px;">';
+			echo '</div>';
+		echo '</div>';
 	}
+	//require_once __DIR__ . '/footer.html';
 }
 ?>
